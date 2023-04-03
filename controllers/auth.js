f1bb2faf23ef7326c5b3e36ba954ea08284ebaca
Witsaroot <pccphet77@gmail.com>
2023-04-03 23:04:25 +0700

    Assignment10 API Security && OpenAPI Swagger

--- controllers/auth.js
+++ controllers/auth.js
@@ -31,6 +31,7 @@ exports.register = async (req, res, next) =>{
 //@route    POST /api/v1/auth/login
 //@access   Public
 exports.login = async (req, res, next) =>{
+    try{
     const {email, password} = req.body;
 
     //Validate email & password
@@ -56,7 +57,9 @@ exports.login = async (req, res, next) =>{
     //const token = user.getSignedJwtToken();
     //res.status(200).json({success: true, token});
     sendTokenResponse(user, 200, res);
-
+    }catch(err){
+        return res.status(401).json({success:false, msg:'Cannot convert email or password to string'});
+    }
 };
 
 //Get token from model, create cookie and send response
@@ -92,5 +95,19 @@ exports.getMe = async (req, res, next) => {
     res.status(200).json({
         success: true,
         data: user
+    });
+};
+
+//@desc Log user out / clear cookie
+//@route GET /api/v1/auth/logout
+//@access Private
+exports.logout=async(req,res,next)=>{
+    res.cookie('token','none',{
+        expires: new Date(Date.now()+ 10*1000),
+        httpOnly:true
+    });
+    res.status(200).json({
+        success:true,
+        data:{}
     });
 };
